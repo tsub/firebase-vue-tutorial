1cc3426a44dcf7cb7abd1e2243ddc49a4cc31e26
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-08-05 11:48:49 +0900

    Implement 4.4

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+import * as firebase from 'firebase';
 import Home from './components/Home.vue'
 import Editor from './components/Editor.vue'
 
@@ -15,6 +16,17 @@ export default {
     return {
       isLogin: false
     }
+  },
+  created: function() {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user)
+
+      if (user) {
+        this.isLogin = true
+      } else {
+        this.isLogin = false
+      }
+    })
   },
   components: {
     Home,
