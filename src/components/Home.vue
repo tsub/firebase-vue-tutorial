3ae1d48a4e1c304d5ffffc3d197f3c75ab485228
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-08-05 11:44:39 +0900

    Implement 4.3

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="home">
     <h1>{{ msg }}</h1>
-    <button>Googleアカウントでログイン</button>
+    <button @click="googleLogin">Googleアカウントでログイン</button>
   </div>
 </template>
 
 <script>
+import * as firebase from 'firebase';
+
 export default {
   name: "home",
   data() {
     return {
       msg: "Welcome to MyMarkdown"
+    }
+  },
+  methods: {
+    googleLogin: function() {
+      firebase
+        .auth()
+        .signInWithRedirect(new firebase.auth.GoogleAuthProvider())
     }
   }
 }
