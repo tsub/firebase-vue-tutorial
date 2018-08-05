812be39cf215fa6bf169ce57d83c5461c7bdca7b
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-08-05 15:38:01 +0900

    Remove console.log

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -21,8 +21,6 @@ export default {
   },
   created: function() {
     firebase.auth().onAuthStateChanged(user => {
-      console.log(user)
-
       if (user) {
         this.isLogin = true
         this.userData = user
