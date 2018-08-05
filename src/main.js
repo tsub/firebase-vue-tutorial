920d382a26e9b4788f7f9a334fbe5fca79616d1e
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-08-05 11:27:49 +0900

    Initial firebase config
    > Only variables that start with VUE_APP_ will be statically embedded into the client bundle with webpack.DefinePlugin. You can access them in your application code:
    
    see https://cli.vuejs.org/guide/mode-and-env.html#using-env-variables-in-client-side-code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as firebase from 'firebase';
+
+const {
+  VUE_APP_FIREBASE_API_KEY,
+  VUE_APP_AUTH_DOMAIN,
+  VUE_APP_DATABASE_URL,
+  VUE_APP_PROJECT_ID,
+  VUE_APP_STORAGE_BUCKET,
+  VUE_APP_MESSAGING_SENDER_ID
+} = process.env
+
+const config = {
+  apiKey: VUE_APP_FIREBASE_API_KEY,
+  authDomain: VUE_APP_AUTH_DOMAIN,
+  databaseURL: VUE_APP_DATABASE_URL,
+  projectId: VUE_APP_PROJECT_ID,
+  storageBucket: VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: VUE_APP_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
 
 Vue.config.productionTip = false
 
